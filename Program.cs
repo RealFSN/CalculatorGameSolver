db60209b8c28014d00d61e69032bebbb7866cab3
RealFSN <moraikhiks@gmail.com>
2024-12-29 19:22:35 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculatorGameSolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
