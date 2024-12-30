181f6240e2eda90b146962c11eb9804044e841b0
RealFSN <moraikhiks@gmail.com>
2024-12-30 16:44:06 +0300

    Added the Remove last digit operation

---
+++ Operations/RemoveLastDigitOperation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorGameSolver.Operations
+{
+    public class RemoveLastDigitOperation : Operation
+    {
+        public RemoveLastDigitOperation()
+        {
+            // this is how it looks in the game
+            base.Name = "<<";
+        }
+
+        public override double Execute(double input)
+        {
+            // similar to Mathf.Abs(input) < 10. just square both sides
+            if (input * input < 100)
+            {
+                return 0;
+            }
+
+            string str = input.ToString();
+            char[] c = str.ToCharArray();
+            char[] newC = new char[c.Length - 1];
+
+            for (int i = 0; i < newC.Length; i++)
+            {
+                newC[i] = c[i];
+            }
+
+            string newStr = new string(newC);
+            return double.Parse(newStr);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,20 +8,21 @@ internal class Program
     static void Main(string[] args)
     {
         int initialNum = 0;
-        int numberOfMoves = 7;
-        int goal = -43;
+        int numberOfMoves = 8;
+        int goal = 1000;
 
         List<Operation> operations = new List<Operation>()
         {
-            new SubtractConstantOperation(5),
-            new SubtractConstantOperation(9),
-            new AddConstantOperation(7),
-            new ReverseOperation()
+            new PushConstantOperation(2),
+            new PushConstantOperation(5),
+            new AddConstantOperation(5),
+            new SubtractConstantOperation(5)
         };
 
         var perms = GenerateAllPermutationsWithRepetition(operations, numberOfMoves);
 
         List<int> solutions = new();
+        List<int> stepsToGetSolution = new();
 
         for (int i = 0; i < perms.Count; i++)
         {
@@ -30,23 +31,27 @@ internal class Program
             for (int j = 0; j < p.Count; j++)
             {
                 val = p[j].Execute(val);
-            }
 
-            if (val == goal)
-            {
-                solutions.Add(i);
+                if (val == goal)
+                {
+                    solutions.Add(i);
+                    stepsToGetSolution.Add(j);
+                }
             }
         }
 
         for (int i = 0; i < solutions.Count; i++)
         {
             var perm = perms[solutions[i]];
-            for (int j = 0; j < perm.Count; j++)
+            Console.WriteLine("Number of steps taken: " + (stepsToGetSolution[i] + 1));
+            for (int j = 0; j <= stepsToGetSolution[i]; j++)
             {
                 Console.WriteLine(perm[j].Name);
             }
             Console.WriteLine();
         }
+
+        Console.WriteLine("The Minimum Number of Steps to Get The Answer is: " + (stepsToGetSolution.Min() + 1));
     }
 
     static List<List<Operation>> GenerateAllPermutationsWithRepetition(List<Operation> ops, int n)
